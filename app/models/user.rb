36aa1fec54d7fe244b90eef47e33264287da9eea
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-20 15:14:10 -0600

    Everything works dont look under the hood...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :favorites
+  has_many :favorite_restaurants, through: :favorites, source: :restaurant
   # Remember to create a migration!
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
